5e48c596ec9c940fdb5cf24e5bb1faa294385816
rkessal <kessal.rayhan25@gmail.com>
2022-12-18 01:41:54 +0100

    cors

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,19 @@ const port = config.PORT;
 const app = express();
 app.use(express.json());
 app.use(expressSession);
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 app.get("/api/healthcheck", (req: Request, res: Response) => {
   console.log("SESSION: " + req.session.userId);
+  res.send(JSON.stringify("Hello"));
 });
 
 app.listen(port, async () => {
   logger.info(`App is running at port: ${port}`);
-  console.log(app.get("host"));
   userRoutes(app);
   exerciseRoutes(app);
   sessionRoutes(app);
